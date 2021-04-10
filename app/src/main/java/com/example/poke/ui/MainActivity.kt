526c831e241e07fb1600aaee03b7e2b981fcefa4
imex18 <35682221+imex18@users.noreply.github.com>
2021-04-10 13:07:57 +0100

    Fragments, Navigation - complete

--- app/src/main/java/com/example/poke/ui/MainActivity.kt
+++ app/src/main/java/com/example/poke/ui/MainActivity.kt
@@ -2,11 +2,39 @@ package com.example.poke.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.poke.R
+import com.example.poke.repository.PokemonRepository
+import com.example.poke2.poke2.ui.list.PokemonViewModel
+import com.example.poke2.poke2.ui.list.ViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: PokemonViewModel
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val repository = PokemonRepository()
+        val viewModelFactory = ViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(PokemonViewModel::class.java)
+
+        setupActionBarWithNavController(findNavController(R.id.fragment_container))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onSupportNavigateUp():Boolean {
+        val navController = findNavController(R.id.fragment_container)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
---
+++ app/src/main/java/com/example/poke/ui/details/DetailsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.poke.ui.details
+
+
+import androidx.fragment.app.Fragment
+import com.example.poke.R
+
+class DetailsFragment : Fragment(R.layout.fragment_details) {
+
+
+
+}
